4ae7db5e3400ddb75f46d67e988a0a66a12fc4b0
Simple-temp <fiveriam01@gmail..com>
2022-06-23 22:38:25 +0600

    forget password api created

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -9,36 +9,36 @@ const User = mongoose.model("User")
 const userRoutes = express.Router()
 
 userRoutes.post("/signup", expressAsyncHandler(async (req, res) => {
-   
-       const { formData } = req.body
-   
-       const newuser = new User({
-           name: formData.name,
-           email: formData.email,
-           password: bcrypt.hashSync(formData.password),
-       })
-       console.log(newuser)
-       const user = await newuser.save()
-   
-       res.send({
-           _id: user._id,
-           name: user.name,
-           email: user.email,
-           isAdmin: user.isAdmin,
-           token: generateToken(user)
-       })
-   }
-
-))
-
-userRoutes.post("/signin", async(req, res) =>{
 
     const { formData } = req.body
 
-    const user = await User.findOne ({ email : formData.email })
+    const newuser = new User({
+        name: formData.name,
+        email: formData.email,
+        password: bcrypt.hashSync(formData.password),
+    })
+    console.log(newuser)
+    const user = await newuser.save()
 
-    if(user){
-        if(bcrypt.compareSync(formData.password, user.password)){
+    res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user)
+    })
+}
+
+))
+
+userRoutes.post("/signin", async (req, res) => {
+
+    const { formData } = req.body
+
+    const user = await User.findOne({ email: formData.email })
+
+    if (user) {
+        if (bcrypt.compareSync(formData.password, user.password)) {
             res.send({
                 _id: user._id,
                 name: user.name,
@@ -53,16 +53,16 @@ userRoutes.post("/signin", async(req, res) =>{
 
 })
 
-userRoutes.put("/updateprofile",isAuth, async (req, res)=>{
+userRoutes.put("/updateprofile", isAuth, async (req, res) => {
 
     const { formData } = req.body
     console.log(req.user._id)
 
-    const user = await User.findById (req.user._id)
-    if(user){
+    const user = await User.findById(req.user._id)
+    if (user) {
         user.name = formData.name || user.name
         user.email = formData.email || user.email
-        if(formData.password){
+        if (formData.password) {
             user.password = bcrypt.hashSync(formData.password, 8)
         }
         console.log(user)
@@ -74,10 +74,31 @@ userRoutes.put("/updateprofile",isAuth, async (req, res)=>{
             isAdmin: updateUser.isAdmin,
             token: generateToken(updateUser)
         })
-    }else {
+    } else {
         res.status(404).send({ message: "User not found" })
     }
 
 })
+
+userRoutes.put("/:email", expressAsyncHandler(async (req, res) => {
+
+    const user = await User.findOne({ email: req.params.email })
+    if (user) {
+        if (req.body.password) {
+            user.password = bcrypt.hashSync(req.body.password, 8) || user.password
+        }
+        const updateUser = await user.save()
+        res.send({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+            token: generateToken(updateUser)
+        })
+    } else {
+        res.status(404).send({ message: "User not found" })
+    }
+
+}))
 
 export default userRoutes;
